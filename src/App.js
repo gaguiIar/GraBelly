3ee14027a37f5d62ddc52715b5ea1997c456ef91
g <gfaguila@usc.edu>
2022-11-07 14:16:22 -0800

    anayltics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
 
-        <img src="https://media2.giphy.com/media/0O5NwsKlbgN2EF3q6x/giphy.gif?cid=6c09b9529jjkoox2x0q3dkr3bryy0ka2ynew0r3gftrwlcex&rid=giphy.gif&ct=s" width= "500px"></img>
+        <img src="https://media2.giphy.com/media/0O5NwsKlbgN2EF3q6x/giphy.gif?cid=6c09b9529jjkoox2x0q3dkr3bryy0ka2ynew0r3gftrwlcex&rid=giphy.gif&ct=s" width= "500px" alt="spinning globe gif"></img>
         <h1>hello world</h1>
     </div>
   );
